2b959097da7c3ab9cae1cafa2d73a68bb819787b
Peter Morris <mrpmorris@gmail.com>
2026-03-29 14:22:01 +0100

    Restart

--- Source/Lib/Morris.FeatureExplorer/Consts.cs
+++
@@ -1,8 +0,0 @@
-using System.IO;
-
-namespace Morris.FeatureExplorer;
-
-internal static class Consts
-{
-	public static readonly string FeaturesPath = $"Features{Path.DirectorySeparatorChar}";
-}
--- Source/Lib/Morris.FeatureExplorer/Features/FeatureExplorer/Models/FileNode.cs
+++
@@ -1,9 +0,0 @@
-namespace Morris.FeatureExplorer.Features.FeatureExplorer.Models;
-
-internal class FileNode : NodeBase
-{
-	public FileNode(string name, string relativePath)
-		: base(name, relativePath)
-	{
-	}
-}
